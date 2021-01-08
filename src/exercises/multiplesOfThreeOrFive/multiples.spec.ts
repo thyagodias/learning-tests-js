f8258b6b4dfcddaefdb397ea33cc6c519b32508b
thyagodias <thyagojuniorrochadias@gmail.com>
2021-01-08 10:28:57 -0400

    feat: adiciona tests para módulo de múltiplos

--- src/exercises/multiplesOfThreeOrFive/multiples.spec.ts
+++ src/exercises/multiplesOfThreeOrFive/multiples.spec.ts
@@ -1,24 +1,21 @@
-import Multiples from './multiples'
-describe('Multiples of Three or/and Five', () => {
+import { checkIfIsMultiples, calculateMultipleBaseOnRuleFunction } from './multiples'
+describe('Multiples', () => {
   describe('Verifica se é multiplo', () => {
-    it('deve retorna todos os números de 1 a 10 que são multiplos de 3', () => {
-      const multiples = new Multiples()
-      expect(multiples.caculatesAllMultiplesUpToLimitValue([3], 10)).toEqual([3, 6, 9])
-    })
-
     it('deve retorna true se 9 for multiplo de 3', () => {
-      const multiples = new Multiples()
-      expect(multiples.checkIfIsMultiples(9, 3)).toBe(true)
+      expect(checkIfIsMultiples(9, 3)).toBe(true)
     })
 
     it('deve retorna false se 3 for multiplo de 5', () => {
-      const multiples = new Multiples()
-      expect(multiples.checkIfIsMultiples(3, 5)).toBe(false)
+      expect(checkIfIsMultiples(3, 5)).toBe(false)
     })
 
-    it('deve retornar os múltiplos de 3 e 5 até 10', () => {
-      const multiples = new Multiples()
-      expect(multiples.caculatesAllMultiplesUpToLimitValue([3, 5], 10)).toEqual([3, 5, 6, 9])
+    it('a soma dos multiplos de 3 ou 5 até 10 deve ser 23', () => {
+      const rule = (num): boolean => {
+        return checkIfIsMultiples(num, 3) || checkIfIsMultiples(num, 5)
+      }
+
+      const sum = calculateMultipleBaseOnRuleFunction(10, rule)
+      expect(sum).toBe(23)
     })
   })
 })
