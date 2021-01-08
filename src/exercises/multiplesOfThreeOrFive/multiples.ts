b7db85db2b9d15954debceb91ad7301206d1d7ce
thyagodias <thyagojuniorrochadias@gmail.com>
2021-01-08 10:29:32 -0400

    refac: altera módulo de multiplos para utilizar high order funtions

--- src/exercises/multiplesOfThreeOrFive/multiples.ts
+++ src/exercises/multiplesOfThreeOrFive/multiples.ts
@@ -1,19 +1,15 @@
-import { removeDuplicateValuesInArray } from '../../helpers/arrayHelper'
+export const calculateMultipleBaseOnRuleFunction = (maxValue: number, rule: Function): number => {
+  let counter = 0
 
-class Multiples {
-  caculatesAllMultiplesUpToLimitValue (multiples: number[], maxValue: number): number[] {
-    const valuesMultiples = []
-    for (let i = 1; i < maxValue; i++) {
-      multiples.forEach(multiple => {
-        if (this.checkIfIsMultiples(i, multiple)) valuesMultiples.push(i)
-      })
+  for (let i = 1; i < maxValue; i++) {
+    if (rule(i)) {
+      counter = counter + i
     }
-    return removeDuplicateValuesInArray(valuesMultiples)
   }
 
-  checkIfIsMultiples (valor: number, multiplo: number): boolean {
-    return valor % multiplo === 0
-  }
+  return counter
 }
 
-export default Multiples
+export const checkIfIsMultiples = (valor: number, multiplo: number): boolean => {
+  return valor % multiplo === 0
+}
