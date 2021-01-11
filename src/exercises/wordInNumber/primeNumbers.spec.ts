de55424855f52c95a4aad925c893f0f7a1e364c9
thyagodias <thyagojuniorrochadias@gmail.com>
2021-01-11 08:43:00 -0400

    feat: adiciona testes do módulo de números primos

---
+++ src/exercises/wordInNumber/primeNumbers.spec.ts
@@ -0,0 +1,22 @@
+import { checkNumberIsPrime } from './primeNumbers'
+
+describe('Prime numbers', () => {
+  it('deve retornar false ao verificar se 1 é um número primo', () => {
+    expect(checkNumberIsPrime(1)).toBe(false)
+  })
+  it('deve retornar false ao verificar se 0 é um número primo', () => {
+    expect(checkNumberIsPrime(0)).toBe(false)
+  })
+  it('deve retornar false ao verificar se -1 é um número primo', () => {
+    expect(checkNumberIsPrime(-1)).toBe(false)
+  })
+  it('deve retornar true ao verificar se 2 é um número primo', () => {
+    expect(checkNumberIsPrime(2)).toBe(true)
+  })
+  it('deve retornar false ao verificar se 10 é um número primo', () => {
+    expect(checkNumberIsPrime(10)).toBe(false)
+  })
+  it('deve retornar true ao verificar se 199 é um número primo', () => {
+    expect(checkNumberIsPrime(199)).toBe(true)
+  })
+})
