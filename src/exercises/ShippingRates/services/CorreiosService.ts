26e2d9b5a7d43c75db72f2cec1bc900a4f7b1a69
thyagodias <thyagojuniorrochadias@gmail.com>
2021-01-14 11:10:16 -0400

    feat: adiciona estrutura de simulação do serviço dos correios

---
+++ src/exercises/ShippingRates/services/CorreiosService.ts
@@ -0,0 +1,7 @@
+import CorreiosShippingService from "../contracts/CorreiosShippingService";
+
+export default class CorreiosService implements CorreiosShippingService {
+  getCorreiosShippingRates (zipCode: string) {
+    return 0
+  }
+}
